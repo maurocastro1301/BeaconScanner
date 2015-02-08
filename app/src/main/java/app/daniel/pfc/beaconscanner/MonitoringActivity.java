2618f2d209f08f1542b7f53b9116b16f543f5d67
GITdanieljc <danigetafe90@gmail.com>
2015-02-08 18:08:39 +0100

    Reference app working

---
+++ app/src/main/java/app/daniel/pfc/beaconscanner/MonitoringActivity.java
@@ -0,0 +1,94 @@
+package app.daniel.pfc.beaconscanner;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+
+/**
+ * 
+ * @author dyoung
+ * @author Matt Tyler
+ */
+public class MonitoringActivity extends Activity  {
+	protected static final String TAG = "MonitoringActivity";
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		Log.d(TAG, "onCreate");
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_monitoring);
+		verifyBluetooth();
+        logToDisplay("Application just launched" );
+	}
+	
+	public void onRangingClicked(View view) {
+		Intent myIntent = new Intent(this, RangingActivity.class);
+		this.startActivity(myIntent);
+	}
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        ((BeaconApp) this.getApplicationContext()).setMonitoringActivity(this);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        ((BeaconApp) this.getApplicationContext()).setMonitoringActivity(null);
+    }
+
+	private void verifyBluetooth() {
+
+		try {
+			if (!BeaconManager.getInstanceForApplication(this).checkAvailability()) {
+				final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+				builder.setTitle("Bluetooth not enabled");			
+				builder.setMessage("Please enable bluetooth in settings and restart this application.");
+				builder.setPositiveButton(android.R.string.ok, null);
+				builder.setOnDismissListener(new DialogInterface.OnDismissListener() {
+					@Override
+					public void onDismiss(DialogInterface dialog) {
+						finish();
+			            System.exit(0);					
+					}					
+				});
+				builder.show();
+			}			
+		}
+		catch (RuntimeException e) {
+			final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+			builder.setTitle("Bluetooth LE not available");			
+			builder.setMessage("Sorry, this device does not support Bluetooth LE.");
+			builder.setPositiveButton(android.R.string.ok, null);
+			builder.setOnDismissListener(new DialogInterface.OnDismissListener() {
+
+				@Override
+				public void onDismiss(DialogInterface dialog) {
+					finish();
+		            System.exit(0);					
+				}
+				
+			});
+			builder.show();
+			
+		}
+		
+	}	
+
+    public void logToDisplay(final String line) {
+    	runOnUiThread(new Runnable() {
+    	    public void run() {
+    	    	EditText editText = (EditText)MonitoringActivity.this
+    					.findViewById(R.id.monitoringText);
+       	    	editText.append(line+"\n");            	    	    		
+    	    }
+    	});
+    }
+
+}
